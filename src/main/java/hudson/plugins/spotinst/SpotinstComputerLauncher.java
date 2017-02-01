b08c2d00e2b51f9b050a468addec512da46252c7
ohadmuchnik <ohad@spotinst.com>
2017-02-01 23:09:53 +0200

    add support  for JNLP launcher

--- src/main/java/hudson/plugins/spotinst/SpotinstComputerLauncher.java
+++ src/main/java/hudson/plugins/spotinst/SpotinstComputerLauncher.java
@@ -2,6 +2,7 @@ package hudson.plugins.spotinst;
 
 import hudson.model.TaskListener;
 import hudson.slaves.ComputerLauncher;
+import hudson.slaves.JNLPLauncher;
 import hudson.slaves.SlaveComputer;
 
 import java.io.IOException;
@@ -9,9 +10,10 @@ import java.io.IOException;
 /**
  * Created by ohadmuchnik on 25/05/2016.
  */
-public class SpotinstComputerLauncher extends ComputerLauncher {
+public class SpotinstComputerLauncher extends JNLPLauncher {
 
     @Override
-    public void launch(SlaveComputer computer, TaskListener listener) throws IOException, InterruptedException {
+    public void launch(SlaveComputer computer, TaskListener listener) {
+        super.launch(computer, listener);
     }
 }
