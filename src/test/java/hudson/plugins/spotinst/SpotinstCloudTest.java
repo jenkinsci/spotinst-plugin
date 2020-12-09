1fe8bbbe37a0f11b4418aa70456eca4e6900b0ae
SHxKM <shibelkm@gmail.com>
2020-12-09 16:49:02 +0200

    [src] unit tests for Azure V3

--- src/test/java/hudson/plugins/spotinst/SpotinstCloudTest.java
+++ src/test/java/hudson/plugins/spotinst/SpotinstCloudTest.java
@@ -1,19 +1,20 @@
 package hudson.plugins.spotinst;
 
+import hudson.model.Node;
 import hudson.plugins.spotinst.api.infra.ApiResponse;
 import hudson.plugins.spotinst.cloud.*;
 import hudson.plugins.spotinst.common.*;
 import hudson.plugins.spotinst.common.AwsInstanceTypeEnum;
 import hudson.plugins.spotinst.model.aws.AwsScaleResultNewSpot;
 import hudson.plugins.spotinst.model.aws.AwsScaleUpResult;
+import hudson.plugins.spotinst.model.azure.AzureV3ScaleResultNewVm;
+import hudson.plugins.spotinst.model.azure.AzureV3VmSizeEnum;
 import hudson.plugins.spotinst.model.gcp.GcpMachineType;
 import hudson.plugins.spotinst.model.gcp.GcpResultNewInstance;
 import hudson.plugins.spotinst.model.gcp.GcpScaleUpResult;
-import hudson.plugins.spotinst.repos.IAwsGroupRepo;
-import hudson.plugins.spotinst.repos.IAzureGroupRepo;
-import hudson.plugins.spotinst.repos.IGcpGroupRepo;
-import hudson.plugins.spotinst.repos.RepoManager;
+import hudson.plugins.spotinst.repos.*;
 import hudson.plugins.spotinst.slave.SlaveUsageEnum;
+import jenkins.model.Jenkins;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -24,6 +25,7 @@ import org.mockito.Mockito;
 import java.util.*;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 /**
  * Created by ohadmuchnik on 19/03/2017.
@@ -35,13 +37,15 @@ public class SpotinstCloudTest {
 
     @Before
     public void setUp() {
-        IAwsGroupRepo   awsGroupRepo   = Mockito.mock(IAwsGroupRepo.class);
-        IGcpGroupRepo   gcpGroupRepo   = Mockito.mock(IGcpGroupRepo.class);
-        IAzureGroupRepo azureGroupRepo = Mockito.mock(IAzureGroupRepo.class);
+        IAwsGroupRepo     awsGroupRepo     = Mockito.mock(IAwsGroupRepo.class);
+        IGcpGroupRepo     gcpGroupRepo     = Mockito.mock(IGcpGroupRepo.class);
+        IAzureGroupRepo   azureGroupRepo   = Mockito.mock(IAzureGroupRepo.class);
+        IAzureV3GroupRepo azureV3GroupRepo = Mockito.mock(IAzureV3GroupRepo.class);
 
         RepoManager.getInstance().setAwsGroupRepo(awsGroupRepo);
         RepoManager.getInstance().setGcpGroupRepo(gcpGroupRepo);
         RepoManager.getInstance().setAzureGroupRepo(azureGroupRepo);
+        RepoManager.getInstance().setAzureV3GroupRepo(azureV3GroupRepo);
 
         SpotinstContext.getInstance().setSpotinstToken("TOKEN");
 
@@ -178,6 +182,77 @@ public class SpotinstCloudTest {
         Mockito.verify(RepoManager.getInstance().getAzureGroupRepo(), Mockito.times(1))
                .scaleUp(groupCapture.capture(), unitsCapture.capture(), accountIdCapture.capture());
         assertEquals(unitsCapture.getValue().intValue(), 2);
+    }
+    //endregion
+
+    //region Azure V3
+    @Test
+    public void testAzureV3Provision_whenThereArePendingInstancesForAllExecutors_thenShouldNotScaleUp() {
+        String groupId = "sig-1";
+        BaseSpotinstCloud spotinstCloud =
+                new AzureV3SpotinstCloud(groupId, "", "20", "/tmp", null, "", false, true, "", null, null, null);
+        Map<String, PendingInstance> pendingInstances = new HashMap<>();
+        pendingInstances.put("vm-1", buildPendingInstance("vm-1", PendingInstance.StatusEnum.PENDING, 2));
+        spotinstCloud.setPendingInstances(pendingInstances);
+        spotinstCloud.provision(null, 2);
+        Mockito.verify(RepoManager.getInstance().getAzureV3GroupRepo(), Mockito.never())
+               .scaleUp(Mockito.anyString(), Mockito.anyInt(), Mockito.anyString());
+    }
+
+    @Test
+    public void testAzureV3Provision_whenThereArePendingInstancesForPartOfTheExecutors_thenShouldScaleUpTheRest() {
+        String groupId = "sig-1";
+        BaseSpotinstCloud spotinstCloud =
+                new AzureV3SpotinstCloud(groupId, "", "20", "/tmp", null, "", false, true, "", null, null, null);
+        Map<String, PendingInstance> pendingInstances = new HashMap<>();
+        pendingInstances.put("vm-1", buildPendingInstance("vm-1", PendingInstance.StatusEnum.PENDING, 2));
+        spotinstCloud.setPendingInstances(pendingInstances);
+
+        AzureV3ScaleResultNewVm newSpot = new AzureV3ScaleResultNewVm();
+        newSpot.setVmName("vm-2");
+        newSpot.setLifeCycle("spot");
+        newSpot.setVmSize(AzureV3VmSizeEnum.STANDARD_A1_V2.getValue());
+
+        List<AzureV3ScaleResultNewVm> vms = Collections.singletonList(newSpot);
+
+        Mockito.when(RepoManager.getInstance().getAzureV3GroupRepo()
+                                .scaleUp(Mockito.anyString(), Mockito.anyInt(), Mockito.anyString()))
+               .thenReturn(new ApiResponse<>(vms));
+
+        spotinstCloud.provision(null, 4);
+
+        ArgumentCaptor<String>  groupCapture     = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<Integer> unitsCapture     = ArgumentCaptor.forClass(Integer.class);
+        ArgumentCaptor<String>  accountIdCapture = ArgumentCaptor.forClass(String.class);
+
+        Mockito.verify(RepoManager.getInstance().getAzureV3GroupRepo(), Mockito.times(1))
+               .scaleUp(groupCapture.capture(), unitsCapture.capture(), accountIdCapture.capture());
+
+        assertEquals(unitsCapture.getValue().intValue(), 2);
+    }
+
+    @Test
+    public void testAzureV3Provision_whenUnrecognizedVmSize_thenDefaultTo1Executor() {
+        String groupId = "sig-1";
+        BaseSpotinstCloud spotinstCloud =
+                new AzureV3SpotinstCloud(groupId, "", "20", "/tmp", null, "", false, true, "", null, null, null);
+
+        AzureV3ScaleResultNewVm newSpot = new AzureV3ScaleResultNewVm();
+        newSpot.setVmName("vm-2");
+        newSpot.setLifeCycle("spot");
+        newSpot.setVmSize("iDontExistType");
+
+        List<AzureV3ScaleResultNewVm> vms = Collections.singletonList(newSpot);
+
+        Mockito.when(RepoManager.getInstance().getAzureV3GroupRepo()
+                                .scaleUp(Mockito.anyString(), Mockito.anyInt(), Mockito.anyString()))
+               .thenReturn(new ApiResponse<>(vms));
+
+        spotinstCloud.provision(null, 4);
+
+        Node node = Jenkins.get().getNode("vm-2");
+        assertNotEquals(node, null);
+        assertEquals(1, node.getNumExecutors());
     }
     //endregion
 
