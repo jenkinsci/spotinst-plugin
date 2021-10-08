149f4d035eba7b5492dfff099d77089933c6f20d
liron.arad <liron.arad@spot.io>
2021-10-08 13:04:30 +0300

    working version - UI GlobalConfiguration

--- src/main/java/hudson/plugins/spotinst/cloud/SpotinstTokenConfig.java
+++ src/main/java/hudson/plugins/spotinst/cloud/SpotinstTokenConfig.java
@@ -1,13 +1,25 @@
 package hudson.plugins.spotinst.cloud;
 
+import com.cloudbees.plugins.credentials.CredentialsMatchers;
+import com.cloudbees.plugins.credentials.common.StandardListBoxModel;
 import hudson.Extension;
+import hudson.model.ItemGroup;
 import hudson.plugins.spotinst.api.SpotinstApi;
+import hudson.plugins.spotinst.common.CredentialsMethodEnum;
 import hudson.plugins.spotinst.common.SpotinstContext;
+import hudson.plugins.spotinst.credentials.SpotTokenCredentialsLoader;
+import hudson.security.ACL;
 import hudson.util.FormValidation;
+import hudson.util.ListBoxModel;
 import jenkins.model.GlobalConfiguration;
+import jenkins.model.Jenkins;
 import net.sf.json.JSONObject;
+import org.kohsuke.stapler.AncestorInPath;
 import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.interceptor.RequirePOST;
+
+import java.util.Collections;
 
 /**
  * Created by ohadmuchnik on 18/07/2016.
@@ -17,6 +29,8 @@ public class SpotinstTokenConfig extends GlobalConfiguration {
     //region Members
     private String spotinstToken;
     private String accountId;
+    private CredentialsMethodEnum credentialsMethod;
+    private String                credentialsId;
     //endregion
 
     public SpotinstTokenConfig() {
@@ -81,5 +95,30 @@ public class SpotinstTokenConfig extends GlobalConfiguration {
     public void setAccountId(String accountId) {
         this.accountId = accountId;
         SpotinstContext.getInstance().setAccountId(accountId);
+    }
+
+    public void setCredentialsMethod(CredentialsMethodEnum credentialsMethod) {
+        this.credentialsMethod = credentialsMethod;
+    }
+
+    public void setCredentialsId(String credentialsId) {
+        this.credentialsId = credentialsId;
+    }
+
+    public CredentialsMethodEnum getCredentialsMethod() {
+        return credentialsMethod;
+    }
+
+    public String getCredentialsId() {
+        return credentialsId;
+    }
+
+    @RequirePOST
+    public ListBoxModel doFillCredentialsIdItems(@AncestorInPath ItemGroup context) {
+        Jenkins.get().checkPermission(Jenkins.ADMINISTER);
+        return new StandardListBoxModel()
+                .includeEmptyValue()
+                .includeMatchingAs(ACL.SYSTEM, context, SpotTokenCredentialsLoader.class, Collections.emptyList(), CredentialsMatchers
+                        .always());
     }
 }
