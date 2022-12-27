645c97bdef4d6e2eaa3d70d47ee1dcbb9f20ee71
sitay <sitay@netapp.com>
2022-12-27 16:13:13 +0200

    InstanceType_by_user_input remove redundant note

--- src/main/java/hudson/plugins/spotinst/common/AwsInstanceTypeSearchMethodEnum.java
+++ src/main/java/hudson/plugins/spotinst/common/AwsInstanceTypeSearchMethodEnum.java
@@ -1,10 +1,15 @@
 package hudson.plugins.spotinst.common;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public enum AwsInstanceTypeSearchMethodEnum {
     SELECT("SELECT"),
     SEARCH("SEARCH");
 
     private final String name;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AwsInstanceTypeSearchMethodEnum.class);
 
     AwsInstanceTypeSearchMethodEnum(String name) {
         this.name = name;
@@ -12,5 +17,21 @@ public enum AwsInstanceTypeSearchMethodEnum {
 
     public String getName() {
         return name;
+    }
+
+    public static AwsInstanceTypeSearchMethodEnum fromValue(String value) {
+        AwsInstanceTypeSearchMethodEnum retVal = null;
+        for (AwsInstanceTypeSearchMethodEnum searchMethodEnum : AwsInstanceTypeSearchMethodEnum.values()) {
+            if (searchMethodEnum.name.equals(value)) {
+                retVal = searchMethodEnum;
+                break;
+            }
+        }
+
+        if (retVal == null) {
+            LOGGER.error("Tried to create search method type enum for: " + value + ", but we don't support such type ");
+        }
+
+        return retVal;
     }
 }
